fe6d0385036477f70b38d99f8655c19a325d7d6f
muhammed <muhammedmuhd7536@gmail.com>
2026-01-30 19:41:00 +0530

    fix react router refresh 404

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState,useEffect } from 'react'
 import axios from 'axios'
 import PageNavbar from '../components/common/PageNavbar'
+import Footer from '../components/common/Footer'
 function Products() {
       const [products,setProducts]=useState([])
    
@@ -13,18 +14,26 @@ function Products() {
         .catch((error)=>console.log(error))
     },[])
 
+    if (!products.length) {
+  return <p className="text-center mt-40">Loading...</p>;
+}
+
+
   return (
     <>
     <PageNavbar/>
-    <div className='grid grid-cols-2  sansation-light mt-[7.3rem]'>
+    <div className='grid grid-cols-2  sansation-light pt-28'>
       {products.map((val)=>(
         <div key={val.id} className='flex flex-col items-center border border-gray-300 p-3'>
-         <img src="/products/jackets/1.png" alt="" />
+         <img src="/products/jackets/1.png" alt="" className='bg-gray-100 rounded-xl'/>
          <p className='line-clamp-1'>{val.name}</p>
          <p>₹{val.price}</p>
         </div>
       ))}
+    
     </div>
+    
+      <Footer/>
     </>
   )
 }
